a6122e6ad8c1a89743fc5838b10c3b41a065f6f5
rekursed <mohammad@hexr.com>
2020-05-06 10:21:35 +0100

    Prism code styling added

--- pages/index.js
+++ pages/index.js
@@ -61,10 +61,10 @@ class Home extends React.Component {
                                         <div className="hero-body">
                                             <div className="container">
                                                 <h1 className="title">
-                                                    Hero title
+                                                    I am available for a new role
                                                 </h1>
                                                 <h2 className="subtitle">
-                                                    Hero subtitle
+                                                    contact me through linked in
                                                 </h2>
                                             </div>
                                         </div>
@@ -73,50 +73,43 @@ class Home extends React.Component {
                             </div>
                         </div>
 
-                        <p className="description">
-                            <Link href='/posts/first'><a>To first</a></Link>
-                        </p>
+                        {/*<p className="description">*/}
+                        {/*    <Link href='/posts/first'><a>To first</a></Link>*/}
+                        {/*</p>*/}
 
-                        <div className="grid">
-                            <a href="https://nextjs.org/docs" className="card">
-                                <h3>Documentation &rarr;</h3>
-                                <p>Find in-depth information about Next.js features and API.</p>
-                            </a>
+                        {/*<div className="grid">*/}
+                        {/*    <a href="https://nextjs.org/docs" className="card">*/}
+                        {/*        <h3>Documentation &rarr;</h3>*/}
+                        {/*        <p>Find in-depth information about Next.js features and API.</p>*/}
+                        {/*    </a>*/}
 
-                            <a href="https://nextjs.org/learn" className="card">
-                                <h3>Learn &rarr;</h3>
-                                <p>Learn about Next.js in an interactive course with quizzes!</p>
-                            </a>
+                        {/*    <a href="https://nextjs.org/learn" className="card">*/}
+                        {/*        <h3>Learn &rarr;</h3>*/}
+                        {/*        <p>Learn about Next.js in an interactive course with quizzes!</p>*/}
+                        {/*    </a>*/}
 
-                            <a
-                                href="https://github.com/zeit/next.js/tree/master/examples"
-                                className="card"
-                            >
-                                <h3>Examples &rarr;</h3>
-                                <p>Discover and deploy boilerplate example Next.js projects.</p>
-                            </a>
+                        {/*    <a*/}
+                        {/*        href="https://github.com/zeit/next.js/tree/master/examples"*/}
+                        {/*        className="card"*/}
+                        {/*    >*/}
+                        {/*        <h3>Examples &rarr;</h3>*/}
+                        {/*        <p>Discover and deploy boilerplate example Next.js projects.</p>*/}
+                        {/*    </a>*/}
 
-                            <a
-                                href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                                className="card"
-                            >
-                                <h3>Deploy &rarr;</h3>
-                                <p>
-                                    Instantly deploy your Next.js site to a public URL with Vercel.
-                                </p>
-                            </a>
-                        </div>
+                        {/*    <a*/}
+                        {/*        href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"*/}
+                        {/*        className="card"*/}
+                        {/*    >*/}
+                        {/*        <h3>Deploy &rarr;</h3>*/}
+                        {/*        <p>*/}
+                        {/*            Instantly deploy your Next.js site to a public URL with Vercel.*/}
+                        {/*        </p>*/}
+                        {/*    </a>*/}
+                        {/*</div>*/}
                     </main>
 
                     <footer>
-                        <a
-                            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Powered by{' '}
-                            <img src="/vercel.svg" alt="Vercel Logo" className="logo"/>
-                        </a>
+
                     </footer>
 
 
